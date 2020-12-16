ed24e2e269ee90d6d16c8e5be86cccd961c233e6
정재웅 <woong_jae@MacBook-Pro.local>
2020-12-16 15:29:21 +0900

    huimin code merged

--- app/src/main/java/com/example/trashsolution/Map.java
+++ app/src/main/java/com/example/trashsolution/Map.java
@@ -1,12 +1,20 @@
 package com.example.trashsolution;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class Map extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +22,35 @@ public class Map extends AppCompatActivity {
         setContentView(R.layout.activity_map);
         Intent intent = getIntent();
         String usr_id = intent.getStringExtra("usr_id");
-        //OOO님 환영합니다!
         String usr_password = intent.getStringExtra("usr_password");
-        Toast.makeText(getApplicationContext(), usr_id+"님, 환영합니다!", Toast.LENGTH_LONG).show();
+
+        Button QrRead_bt = (Button)findViewById(R.id.QrRead_bt); //QR인식 버튼
+
+        //QR인식 버튼 클릭시 기능
+        QrRead_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myintent = new Intent(Map.this, Qr_Scan.class);
+                startActivity(myintent);
+                //overridePendingTransition(R.anim.fadein, R.anim.fadeout); 효과
+            }
+        });
+    }
+    @Override
+    public void onBackPressed(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("알림");
+        builder.setMessage("종료하시겠습니까?");
+        builder.setNegativeButton("취소",null);
+        builder.setPositiveButton("종료", new DialogInterface.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                moveTaskToBack(true);						// 태스크를 백그라운드로 이동
+                finishAndRemoveTask();						// 액티비티 종료 + 태스크 리스트에서 지우기
+                android.os.Process.killProcess(android.os.Process.myPid());	// 앱 프로세스 종료
+            }
+        });
+        builder.show();
     }
 }
